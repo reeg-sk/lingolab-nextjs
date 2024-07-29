8db91b4971fd5c5a9f1b730d55c2064d58dfd93d
reeg-sk <fregtvs@gmail.com>
2024-07-29 19:30:15 +0200

    added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,9 +4,10 @@ import siteMetadata from "@/data/siteMetadata";
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
 
-  const routes = ["", "blog", "kurzy", "o-nas", "kontakt"].map((route) => ({
+  const routes = ["", "kurzy", "blog", "o-nas", "kontakt", "hodina-zdarma"].map((route, i) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split("T")[0],
+    priority: 1 - i * 0.1,
   }));
 
   return [...routes];
