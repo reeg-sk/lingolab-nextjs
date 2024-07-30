08e05204f965864d09579ba2607e00fa7f1fdaba
reeg-sk <fregtvs@gmail.com>
2024-07-30 13:20:31 +0200

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,25 @@
 import { MetadataRoute } from "next";
 import siteMetadata from "@/data/siteMetadata";
+import { getLanguages, getLatestPosts } from "@/lib/records";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const siteUrl = siteMetadata.siteUrl;
 
-  const routes = ["", "kurzy", "blog", "o-nas", "kontakt", "hodina-zdarma"].map((route, i) => ({
+  const routes = ["", "kurzy", "hodina-zdarma", "blog", "o-nas", "kontakt"].map((route, i) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split("T")[0],
     priority: 1 - i * 0.1,
   }));
 
-  // load all languages and add them to the sitemap
+  const languages = await getLanguages();
+  languages.forEach((language) => {
+    routes.push({
+      url: `${siteUrl}/kurzy/${language.slug}`,
+      lastModified: new Date().toISOString().split("T")[0],
+      priority: 0.9,
+    });
+  });
+
 
   return [...routes];
 }
