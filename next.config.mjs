973033cc49585258e3e02da26ced05194a4e43d0
Andrej Kazmirsky <andrej@kazmirsky.com>
2024-09-05 17:53:01 +0200

    add docker and workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'ui-avatars.com',
-            port: '',
-            pathname: '/api/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'tailwindui.com',
-            port: '',
-            pathname: '/img/**',
-          },
-        ],
-    }
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
