7e9079f0be6746d2c2fead200ee7aac04110355b
Andrej Kazmirsky <andrej@kazmirsky.com>
2024-11-13 10:27:24 +0100

    change freq question image

--- src/components/section/FreqQuestion.tsx
+++ src/components/section/FreqQuestion.tsx
@@ -8,7 +8,7 @@ export default async function FreqQuestions({ questions }) {
       <div className="relative mx-auto grid lg:grid-cols-2 items-center max-w-2xl lg:max-w-7xl gap-x-8 gap-y-10 md:gap-y-20 px-4 pb-24">
         <div className="overflow-hidden">
           <Image
-            src="https://cdn.lingolab.sk/static/team.jpg"
+            src="https://cdn.lingolab.sk/uploads/56dd341e-cedb-450b-be40-f83f9f58e8c5.jpg"
             alt="Ukážka odpovedí na otázky našimi lektormi"
             className="w-[48rem] object-[-11cm_0] md:object-center max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-mr-32 lg:-ml-96"
             width="2432"
